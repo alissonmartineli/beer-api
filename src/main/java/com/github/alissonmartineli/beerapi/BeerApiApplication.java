cc4abf39bbcc271cd95111e067bac67cf1c369f9
Alisson Martineli <martineli.alisson@gmail.com>
2022-01-22 13:49:52 -0300

    feat: :tada: setup

---
+++ src/main/java/com/github/alissonmartineli/beerapi/BeerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.alissonmartineli.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApiApplication.class, args);
+	}
+
+}
